f698eaa2831e5a88884294da3f96928502022e49
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-10 15:46:13 +0200

    1

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,27 +1,27 @@
 let posts = [
-  { id: "1", topic: "test1", text: "test text1" },
-  { id: "2", topic: "test2", text: "test text2" },
-  { id: "3", topic: "test3", text: "test text3" },
+  {id: '1', topic: 'test1', text: 'test text1'},
+  {id: '2', topic: 'test2', text: 'test text2'},
+  {id: '3', topic: 'test3', text: 'test text3'},
 ];
 
 const getPost = (req, res) => {
-  res.json({ posts, status: "success" });
+  res.json({posts, status: 'success'});
 };
 
 const getPostById = (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   const [post] = posts.filter((el) => el.id === id);
 
   if (!post) {
     return res
-      .status(400)
-      .json({ status: `failure, no post with id "${id}" found!` });
+        .status(400)
+        .json({status: `failure, no post with id "${id}" found!`});
   }
-  res.json({ post, status: "success" });
+  res.json({post, status: 'success'});
 };
 
 const addPost = (req, res) => {
-  const { topic, text } = req.body;
+  const {topic, text} = req.body;
 
   posts.push({
     id: new Date().getTime().toString(),
@@ -29,22 +29,22 @@ const addPost = (req, res) => {
     text,
   });
 
-  res.json({ status: "success" });
+  res.json({status: 'success'});
 };
 
 const changePost = (req, res) => {
-  const { topic, text } = req.body;
+  const {topic, text} = req.body;
   posts.forEach((el) => {
     if (el.id === req.params.id) {
       el.topic = topic;
       el.text = text;
     }
   });
-  res.json({ status: "success" });
+  res.json({status: 'success'});
 };
 
 const patchPost = (req, res) => {
-  const { topic, text } = req.body;
+  const {topic, text} = req.body;
   posts.forEach((el) => {
     if (el.id === req.params.id) {
       if (topic) {
@@ -55,12 +55,12 @@ const patchPost = (req, res) => {
       }
     }
   });
-  res.json({ status: "success" });
+  res.json({status: 'success'});
 };
 
 const deletePost = (req, res) => {
   posts = posts.filter((el) => el.id !== req.params.id);
-  res.json({ status: "success" });
+  res.json({status: 'success'});
 };
 
 module.exports = {
